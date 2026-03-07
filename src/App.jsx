452d01c91d782521901d122b56481ea0c3d6371c
Gavin Pereira <gavinpereira93080@gmail.com>
2026-03-08 02:53:30 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Body from "./components/Body";
+import Head from "./components/Head";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+import store from "./utils/store";
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      }]     
+  }
+]);
 
 function App() {
-
-
   return (
-    <>
-     <h1>Hello world</h1>
-    </>
-  )
+    <Provider store={store}>
+      <div>
+        <Head />
+        <RouterProvider router={appRouter} />
+      </div>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
