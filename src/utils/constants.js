6fdf8e0f730f2d0b243392aceb4be8a77423d239
Gavin Pereira <gavinpereira93080@gmail.com>
2026-03-08 02:57:43 +0530

    Took off API KEYS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
-const GOOGLE_API_KEY = "AIzaSyANKZxBnmM8Hds5FBjCzry-Zp9Kmhmofj4"
+const GOOGLE_API_KEY = ""
 
 export const LIVE_CHAT_COUNT = 25;
 
-// export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyANKZxBnmM8Hds5FBjCzry-Zp9Kmhmofj4";
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
+
 
 
 export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
